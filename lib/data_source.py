700ac8832612fcae489aeb3dc73977e2f2f482b6
Andrew Roth <andrewroth1991@gmail.com>
2014-07-26 21:15:03 -0500

    Added Data Source class and tests
    DataSource will handle creating a generator from a passed in
    json-encoded set of data.
    
    Implements #6

---
+++ lib/data_source.py
@@ -0,0 +1,54 @@
+class DataSource:
+    def __init__(self, item_data, header_data):
+        self.item_data = item_data
+        self.header_data = header_data
+
+    def get_headers(self, headers, *args):
+        """Obtain the indices of desired headers
+
+        If the data is stored as lists rather than dictionaries, with a 
+        separate list storing information about the headers, this function
+        can be used to determine which indices contain the desired headers.
+        
+        Arguments:
+        headers -- a set containing the names of headers to find the indices of
+        *args -- any additional parameters needed to reach the header name.
+                 For example, if your header info is stored as 
+                 [{'meta': {'name': HEADER_NAME}}, ...], then *args should 
+                 be set to ['meta', 'name'].
+        """
+        if not headers:
+            return {}
+
+        header_indices = {}
+        for index, header_info in enumerate(self.header_data):
+            for arg in args:
+                header_info = header_info[arg]
+           
+            if header_info in headers:
+                header_indices[header_info] = index
+
+        return header_indices
+
+    def get_items(self, header_indices, *args):
+        """Generator which yields selected info from an item
+
+        Arguments:
+        header_indices -- a dictionary mapping the name of a header, to the 
+                          index of that component of an item. If the data is
+                          stored as dictionaries, header_indices can be set to
+                          {HEADER: HEADER}
+        *args -- any additional parameters needed to reach an item.
+                 For example, if your data is stored as 
+                 [{'info': {'data': ITEM}}, ...], then *args should be set to
+                 ['info', 'data']
+        """
+        if not header_indices:
+            raise StopIteration
+
+        for item in self.item_data:
+            for arg in args:
+                item = item[arg]
+
+            row = {header: item[index] for header, index in header_indices.items()}
+            yield row
---
+++ tests/test_data_source.py
@@ -0,0 +1,145 @@
+import sys
+import unittest
+
+sys.path.insert(0, '../')
+
+from lib.data_source import DataSource
+
+ITEM_DATA = [
+    [
+        1, 
+        '59D12C01-862C-4FD8-B6E1-53B21A1C03DE', 
+        1, 
+        1406388129, 
+        '403253'
+    ],
+    [
+        2, 
+        '87437A5B-5C7B-444A-BBD5-1803D1B63422', 
+        2, 
+        1406388129, 
+        '403253'
+    ], 
+    [
+        3, 
+        '76CBA7D9-8D51-47FE-B7C6-81437BC4B3A1', 
+        3, 
+        1406388129, 
+        '403253'
+    ], 
+    [
+        4, 
+        '40A9F4FA-BC7E-49F0-9343-7E3EE160FFC7', 
+        4, 
+        1406388129, 
+        '403253'
+    ], 
+    [
+        5, 
+        'D5FF0207-8D91-47DE-AA69-1B7AFE762F8F', 
+        5, 
+        1406388129, 
+        '403253'
+    ]
+]
+
+HEADER_DATA = [
+    {
+        'id' : -1,
+        'name' : 'sid',
+        'dataTypeName' : 'meta_data',
+        'fieldName' : ':sid',
+        'position' : 0,
+        'renderTypeName' : 'meta_data',
+    }, {
+        'id' : -1,
+        'name' : 'id',
+        'dataTypeName' : 'meta_data',
+        'fieldName' : ':id',
+        'position' : 0,
+        'renderTypeName' : 'meta_data',
+    }, {
+        'id' : -1,
+        'name' : 'position',
+        'dataTypeName' : 'meta_data',
+        'fieldName' : ':position',
+        'position' : 0,
+        'renderTypeName' : 'meta_data',
+    }, {
+        'id' : -1,
+        'name' : 'created_at',
+        'dataTypeName' : 'meta_data',
+        'fieldName' : ':created_at',
+        'position' : 0,
+        'renderTypeName' : 'meta_data',
+    }, {
+        'id' : -1,
+        'name' : 'created_meta',
+        'dataTypeName' : 'meta_data',
+        'fieldName' : ':created_meta',
+        'position' : 0,
+        'renderTypeName' : 'meta_data',
+    }
+]
+
+class Test_Data_Source(unittest.TestCase):
+    def setUp(self):
+        self.empty_data_source = DataSource([], [])
+        self.data_source = DataSource(ITEM_DATA, HEADER_DATA)
+
+    def test_get_headers_no_data_no_headers(self):
+        expected = {}
+        actual = self.empty_data_source.get_headers(set([]))
+        self.assertEquals(expected, actual)
+
+    def test_get_headers_no_data(self):
+        expected = {}
+        actual = self.empty_data_source.get_headers(set(['test']))
+        self.assertEquals(expected, actual)
+
+    def test_get_headers(self):
+        expected = {
+            'id': 1,
+            'position': 2,
+        }
+        headers = set(['id', 'position'])
+        actual = self.data_source.get_headers(headers, 'name')
+        self.assertEquals(expected, actual)
+
+    def test_get_headers_no_headers(self):
+        expected = {}
+        actual = self.data_source.get_headers(set([]))
+        self.assertEquals(expected, actual)
+
+    def test_next_no_data_no_headers(self):
+        expected = []
+        actual = [x for x in self.empty_data_source.get_items({})]
+        self.assertEquals(expected, actual)
+
+    def test_next_no_data(self):
+        headers = {'sid': 0}
+
+        expected = []
+        actual = [x for x in self.empty_data_source.get_items(headers)]
+        self.assertEquals(expected, actual)
+
+    def test_next_no_headers(self):
+        expected = []
+        actual = [x for x in self.data_source.get_items({})]
+        self.assertEquals(expected, actual)
+
+    def test_next(self):
+        headers = {'sid': 0}
+
+        expected = [
+            {'sid': 1},
+            {'sid': 2},
+            {'sid': 3},
+            {'sid': 4},
+            {'sid': 5}
+        ]
+        actual = [x for x in self.data_source.get_items(headers)]
+        self.assertEquals(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
