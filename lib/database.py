7e828f18b3a143ec1a0cb0c18e2213e45d4d75be
Andrew Roth <andrewroth1991@gmail.com>
2014-07-28 13:09:13 -0500

    Database class added
    The Database class wraps around sqlite3 and provides methods for
    initializing a database, adding a row, and returning all the foodtrucks
    within a given distance of a given point.
    
    This implements #5.

---
+++ lib/database.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+class DataBase:
+    def __init__(self, database):
+        self.conn = sqlite3.connect(database)
+        self.conn.row_factory = sqlite3.Row
+
+    def __del__(self):
+        self.conn.close()
+
+    def init_database(self):
+        statement = """
+            drop table if exists foodtrucks;
+            create table foodtrucks (
+                id integer primary key autoincrement,
+                name text not null,
+                address text not null,
+                fooditems text not null,
+                latitude real not null,
+                longitude real not null
+            );
+        """
+        cur = self.conn.cursor()
+        cur.executescript(statement)
+        self.conn.commit()
+        cur.close()
+
+    def add_row(self, name, address, fooditems, longitude, latitude):
+        statement = """
+            insert into foodtrucks 
+                (name, address, fooditems, longitude, latitude)
+                values (?, ?, ?, ?, ?)
+        """
+        cur = self.conn.cursor()
+        cur.execute(
+            statement,
+            (name, address, fooditems, latitude, longitude)
+        )
+        self.conn.commit()
+        cur.close()
+
+    def gen_within_distance(self, distance, longitude, latitude):
+        if distance < 0.0:
+            raise ValueError('distance must be a positive number')
+
+        statement = """
+            select name, address, fooditems from foodtrucks
+            where (longitude - ?)*(longitude - ?) + 
+            (latitude - ?)*(latitude - ?) <= ?
+        """
+        cur = self.conn.cursor().execute(
+            statement,
+            (longitude, longitude, latitude, latitude, distance)
+        )
+        for row in cur.fetchall():
+            foodtruck = {key: val for key, val in zip(row.keys(), row)}    
+            yield foodtruck
+        cur.close()
---
+++ tests/test_database.py
@@ -0,0 +1,55 @@
+import sys
+import unittest
+
+sys.path.insert(0, '../')
+
+from lib.database import DataBase
+
+ROWS = [
+    [
+        'test_name_{0}'.format(i), 
+        'test_address_{0}'.format(i), 
+        'test_fooditems_{0}'.format(i), 
+        float(i),
+        0.0
+    ] for i in range(0, 10)
+]
+
+class Test_DataBase(unittest.TestCase):
+    def setUp(self):
+        self.database = DataBase(':memory:')
+        self.database.init_database()
+        for row in ROWS:
+            self.database.add_row(*row)
+
+    def tearDown(self):
+        del self.database
+
+    def test_gen_within_distance(self):
+        expected = [
+            {
+                'name': 'test_name_0',
+                'address': 'test_address_0',
+                'fooditems': 'test_fooditems_0',
+            },
+            {
+                'name': 'test_name_1',
+                'address': 'test_address_1',
+                'fooditems': 'test_fooditems_1',
+            }
+        ]
+
+        actual = [x for x in self.database.gen_within_distance(1.5, 0.0, 0.0)]
+        self.assertEquals(expected, actual)
+
+    def test_gen_within_distance_bad_distance(self):
+        with self.assertRaises(ValueError):
+            [x for x in self.database.gen_within_distance(-1.0, 0.0, 0.0)]
+
+    def test_gen_within_distance_no_results(self):
+        expected = []
+        actual = [x for x in self.database.gen_within_distance(10.0, 100.0, 100.0)]
+        self.assertEquals(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
