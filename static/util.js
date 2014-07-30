3e9c3937542649ba638501a7bf5f19f2966f5625
Andrew Roth <andrewroth1991@gmail.com>
2014-07-30 14:20:42 -0500

    LatLng now looked up from address
    Index.html now presents the user with an address box, which when filled
    in will look up the Lat and Lng using the Google Maps API.
    
    This implements #7.

--- static/util.js
+++ static/util.js
@@ -1,3 +1,13 @@
+/**
+ * Dependencies:
+ * Google Maps: https://maps.googleapis.com/maps/api/js
+ */
+
+/**
+ * obtains GET parameters
+ * @return
+ *    The GET parameters in a dictionary
+ */
 function getUrlParameters()
 {
   var params = {};
@@ -12,6 +22,18 @@ function getUrlParameters()
   return params;
 }
 
+/**
+ * creates and sends a synchronous http request
+ * @param url
+ *    the url to send the request to
+ * @method
+ *    the http method to use
+ * @payload
+ *    any data that should be sent in a POST request. Should be set to null for a GET
+ * @return
+ *    the response of the request
+ */
+
 function httpRequest(url, method, payload)
 {
   var request = new XMLHttpRequest();
@@ -20,18 +42,57 @@ function httpRequest(url, method, payload)
   return request.responseText;
 }
 
-function create_url(params)
+/**
+ * forms a url with GET parameters
+ * @param base_url
+ *    the url to add parameters to
+ * @params
+ *    a dictionary of parameters to add
+ * @return
+ *    the formatted url
+ */
+function create_url(base_url, params)
 {
-  return location.protocol + '//' + location.host + 
-    '/api/foodtrucks?latitude=' + params['latitude'] + 
-    '&longitude=' + params['longitude'] + 
-    '&distance=' + params['distance'];
+  param_list = [];
+  for (index in params) {
+    param_list.push(index + '=' + params[index]);
+  }
+  return base_url + '?' + param_list.join('&');
 }
 
+/**
+ * Checks if a parameter is a number
+ * @param
+ *    The parameter to check
+ * @return
+ *    true if param is a number, false otherwise.
+ */
+function param_is_valid(param)
+{
+  return typeof param == 'number' && param != undefined && !isNaN(param);
+}
+
+/**
+ * Creates an HTML table for the passed in foodtrucks
+ * @param foodtrucks
+ *    an array of dictionaries, with each dictionary containing information for a 
+ *    foodtruck. A foodtruck should have the following parameters: Name, Address, 
+ *    MenuItems.
+ * @return
+ *    the created HTML table
+ */   
 function create_table(foodtrucks)
 {
   var table = document.createElement('table');
-  
+  var border = document.createAttribute('border');
+  border.value = "1";
+  table.setAttributeNode(border);
+  var th = table.createTHead();
+  var tr = th.insertRow();
+  tr.insertCell().appendChild(document.createTextNode('Name'));
+  tr.insertCell().appendChild(document.createTextNode('Address'));
+  tr.insertCell().appendChild(document.createTextNode('Menu items'));
+ 
   for (index in foodtrucks) {
     var tr = table.insertRow();
     tr.insertCell().appendChild(
@@ -51,4 +112,30 @@ function create_table(foodtrucks)
     );
   }
   return table;
+}
+
+/** Requires Google maps
+ * Obtains the latitude and longitude of the passed in address, adds them
+ * to the passed in form, and then submits the form.
+ * @param client_address
+ *     A string containing the address to lookup
+ * @param form
+ *    The form to add the latitude and longitude to
+ */
+function add_latlng_to_form(client_address, form)
+{
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode(
+    {address: client_address},
+    function (results, status) {
+      if (results.length) {
+        var latitude = results[0].geometry.location.k;
+        var longitude = results[0].geometry.location.B;
+        form.latitude.value = latitude;
+        form.longitude.value = longitude;
+        form.address.remove();
+        form.submit();
+      }
+    }
+  );
 }
